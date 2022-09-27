d5f629d8756905bb35a9364797b1bce046791d30
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-26 21:10:18 -0300

    Migrate usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,10 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('cpf')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->double('balance')->nullable();
+            $table->enum('type', ['common', 'shopkeeper']);
             $table->timestamps();
         });
     }
